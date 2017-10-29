bf78957934042b87eb4c4e47b964562bb86e4086
Raissa Ferreira <raissa.ferreira@concrete.com.br>
2017-10-29 00:48:49 -0200

    Mount api routes on specific blueprint

--- banking/app.py
+++ banking/app.py
@@ -1,49 +1,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from decouple import config
-
-from api.healthcheck import HealthcheckResource
-from api.accounts import AccountResource
-from api.clients import ClientResource
-from api.subscriptions import SubscriptionResource
-from api.transactions import TransactionResource
+from flasgger import Swagger
 
 
 db = SQLAlchemy()
 
-
-def mount_routes(app):
-    app.add_url_rule("/api/healthcheck",
-                 view_func=HealthcheckResource.as_view("healthcheck"),
-                 methods=["GET"])
-    app.add_url_rule("/api/accounts",
-                     view_func=AccountResource.as_view("accounts"),
-                     methods=["POST"])
-    app.add_url_rule("/api/accounts/<account_id:int>/transactions",
-                     view_func=TransactionResource.as_view("transactions"),
-                     methods=["GET", "POST"])
-    app.add_url_rule("/api/clients",
-                     view_func=ClientResource.as_view("clients"),
-                     methods=["GET", "POST"])
-    app.add_url_rule("/api/clients/<client_id:int>/accounts",
-                     view_func=AccountResource.as_view("accounts"),
-                     methods=["GET"])
-    app.add_url_rule("/api/subscriptions",
-                     view_func=SubscriptionResource.as_view("subscriptions"),
-                     methods=["POST"])
-    app.add_url_rule("/api/subscriptions/<subscription_id:int>",
-                     view_func=TransactionResource.as_view("subscriptions"),
-                     methods=["DELETE"])
-    app.add_url_rule("/api/accounts/<account_id:int>/transactions",
-                     view_func=TransactionResource.as_view("transactions"),
-                     methods=["GET", "POST"])
 
 def create_app():
     app = Flask(__name__)
     app.config["SQLALCHEMY_DATABASE_URI"] = config("SQLALCHEMY_DATABASE_URI")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = config("SQLALCHEMY_TRACK_MODIFICATIONS", cast=bool, default=False)
     app.config["DEBUG"] = config("DEBUG", cast=bool, default=True)
+    app.config["SWAGGER"] = {
+        "title": "Banking API",
+        "uiversion": 3,
+        "specs_route": "/api/docs/"
+    }
     db.init_app(app)
-    mount_routes(app)
-
+    from .api.blueprint import api
+    app.register_blueprint(api)
+    swagger = Swagger(app)
     return app
